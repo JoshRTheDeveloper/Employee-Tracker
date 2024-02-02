1c4bd832aa59b2211941fb700f5434dde7ad5fc9
Josh Roth <you@example.comraider4414@gmail.com>
2024-02-01 21:56:50 -0600

    Schema updated

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+
+DROP DATABASE IF EXISTS employee_management;
+CREATE DATABASE IF NOT EXISTS employee_management;
+USE employee_management;
+
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id),
+     PRIMARY KEY (id)
+);
+
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id),
+     PRIMARY KEY (id)
+);
+
